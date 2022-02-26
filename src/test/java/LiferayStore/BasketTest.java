2c340bc61e345904c50eeb2a4dd2c281a31c28a9
leylashakir <leylashakir@gmail.com>
2022-02-25 20:30:33 -0500

    Updated tests for Basket.java

--- src/test/java/LiferayStore/BasketTest.java
+++ src/test/java/LiferayStore/BasketTest.java
@@ -3,7 +3,6 @@ package LiferayStore;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -11,27 +10,45 @@ import static org.junit.jupiter.api.Assertions.*;
 class BasketTest {
 
     Basket basket;
+    List<Item> items;
 
     @BeforeEach
     void setUp() {
         basket = new Basket();
         basket.addItem(1, "book", 4.99);
         basket.addItem(1, "music CD", 3.99);
+        basket.addItem(1, "imported chocolate", 3.99);
+        items = basket.getItems();
     }
 
     @Test
-    void addItem() {
-
-        assertNotNull(basket);
-        assertEquals(2, basket.getItems().size());
+    void testAddItem() {
+        assertEquals(3, items.size());
     }
 
     @Test
-    void getItems() {
+    void testGetItems() {
+        assertEquals(items, items);
+    }
 
-        List<Item> items = basket.getItems();
-        assertNotNull(items);
-        assertEquals(2, items.size());
+    @Test
+    void testHasSalesTax() {
+        assertTrue(basket.hasSalesTax(items.get(1)));
+        assertFalse(basket.hasSalesTax(items.get(2)));
+    }
 
+    @Test
+    void testHasImportTax() {
+        assertFalse(basket.hasImportTax(items.get(1)));
+        assertTrue(basket.hasImportTax(items.get(2)));
+    }
+
+    @Test
+    void testRoundAmount() {
+        double amount = 53.33;
+        assertEquals(53.35, Basket.roundAmount(amount));
+
+        amount = 53.31;
+        assertEquals(53.30, Basket.roundAmount(amount));
     }
 }
